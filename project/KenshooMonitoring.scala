9d055ab2320655e08b1a245f9a81606cee8d62ca
Artur Opala <250927+arturopala@users.noreply.github.com>
2019-04-10 16:14:17 +0100

    [AO] migrate to Play 2.6 and Scala 2.12

--- project/KenshooMonitoring.scala
+++ project/KenshooMonitoring.scala
@@ -17,10 +17,6 @@ import sbt.Keys._
 import sbt._
 import uk.gov.hmrc.DefaultBuildSettings._
 import uk.gov.hmrc.PublishingSettings._
-import uk.gov.hmrc.playcrosscompilation.AbstractPlayCrossCompilation
-import uk.gov.hmrc.playcrosscompilation.PlayVersion.Play25
-
-object PlayCrossCompilation extends AbstractPlayCrossCompilation(defaultPlayVersion = Play25)
 
 object KenshooMonitoringBuild extends Build {
   import uk.gov.hmrc.SbtArtifactory.autoImport.makePublicallyAvailableOnBintray
@@ -42,29 +38,19 @@ object KenshooMonitoringBuild extends Build {
 
   lazy val kenshooMonitoring = Project("agent-kenshoo-monitoring", file("."))
     .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning, SbtArtifactory)
-    .settings(majorVersion := 3)
+    .settings(majorVersion := 4)
     .settings(scalaSettings ++ scoverageSettings: _*)
     .settings(defaultSettings(): _*)
     .settings(
       targetJvm := "jvm-1.8",
-      libraryDependencies ++= PlayCrossCompilation.dependencies(
-        shared = Seq(
+      scalaVersion := "2.12.8",
+      libraryDependencies ++= Seq(
           "org.scalatest" %% "scalatest" % "3.0.5" % "test",
-          "org.mockito" % "mockito-core" % "2.23.4" % "test"
-        ),
-        play25 = Seq(
-          "com.kenshoo" %% "metrics-play" % "2.5.9_0.5.1",
-          "uk.gov.hmrc" %% "bootstrap-play-25" % "4.9.0",
-          "uk.gov.hmrc" %% "hmrctest" % "3.6.0-play-25" % "test"
-        ),
-        play26 = Seq(
+          "org.mockito" % "mockito-core" % "2.23.4" % "test",
           "com.kenshoo" %% "metrics-play" % "2.6.19_0.7.0",
           "uk.gov.hmrc" %% "bootstrap-play-26" % "0.37.0",
-          "uk.gov.hmrc" %% "hmrctest" % "3.6.0-play-26" % "test"
-        )
-      ),
-      crossScalaVersions := Seq("2.11.12")
-    )
+          "uk.gov.hmrc" %% "hmrctest" % "3.6.0-play-26" % "test")
+      )
     .settings(publishAllArtefacts : _*)
     .settings(makePublicallyAvailableOnBintray := true)
     .settings(resolvers ++= Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -9,12 +9,10 @@ addSbtPlugin("uk.gov.hmrc" % "sbt-git-versioning" % "1.16.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-artifactory" % "0.17.0")
 
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.19")
+addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.6.21")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.0")
 
 addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.8.0")
 
 addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.16")
-
-addSbtPlugin("uk.gov.hmrc" % "sbt-play-cross-compilation" % "0.15.0")
